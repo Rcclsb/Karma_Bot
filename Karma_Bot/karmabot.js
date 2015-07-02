89682853f10ef830ca52b920a4807020ba47e62c
Roberto Cohen <rcclsb@gmail.com>
2015-07-02 16:22:29 -0400

    changed IP

--- Karma_Bot/karmabot.js
+++ Karma_Bot/karmabot.js
@@ -3,7 +3,7 @@ var negativeKey = encodeURIComponent("--");
 var karmaGetKey = encodeURIComponent("karma value ");
 var slackBotCall = "Karmabot ";
 var redis = require("redis");
-var client = redis.createClient(6379, '52.27.30.0', {parser: "javascript", max_attempts: 5});
+var client = redis.createClient(6379, '52.26.218.235', {parser: "javascript", max_attempts: 5});
 var currentKarmaValue = "null0";
 var messageText;
 var returnText = "defaultReturnText";
